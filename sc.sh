5c6d612cf483952caf864bb5a08d451ac66e0d72
Ahmed Allioui <ahmed.allioui@smail.inf.h-brs.de>
2022-10-25 11:34:30 +0200

    Script for shell created

---
+++ sc.sh
@@ -0,0 +1,9 @@
+init --salesmen 10 --records 2
+
+get --employeeId 1
+
+update --employeeId 1 --firstName "Adam" --lastName "Ali"
+
+create --employeeId 10 --firstName "Alaa" --lastName "Isam"
+
+get-all
